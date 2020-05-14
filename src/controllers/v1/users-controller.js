99539321e786246523423c476b831efd267b1947
Fabricio Palavecino <fabricioleonelpalavecino@gmail.com>
2020-05-14 15:04:39 -0300

    correcion userName

--- src/controllers/v1/users-controller.js
+++ src/controllers/v1/users-controller.js
@@ -1,11 +1,38 @@
 const bcrypt = require("bcryptjs");
+const Users = require("../../mongo/models/users"); 
 
 const createUser = async (req, res) => {
   try {
-    const hash = await bcrypt.hash(req.body.password, 15);
-    console.log("fin", hash);
+    console.log("req.body", req.body);
+
+    const { username, email, password, data } = req.body;
+
+    const hash = await bcrypt.hash(password, 15);
+
+    /*await Users.create({
+      username,
+      email,
+      data,
+      password: hash
+    });*/
+
+    const user = new Users();
+    user.username = username;
+    user.email = email;
+    user.data = data;
+    user.password = hash;
+
+    await user.save();
+
+
+    
     res.send({ status: "OK", message: "user created" });
   } catch (error) {
+    if(error.code && error.code === 11000){
+      res.status(400).send({ status: "DUPLICATED_VALUES", message: error.keyValue });
+      return;
+    }
+    console.log("create user:", error);
     res.status(500).send({ status: "ERROR", message: error.message });
   }
 };
--- src/mongo/models/products.js
+++ src/mongo/models/products.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 
 //const Schema = mongoose.Schema;
 const [Schema] = mongoose;
-const productsSchema = new Schema({
-  title: { type: String, required: true },
-  desc: { type: String, required: true },
-  price: { type: Number, required: true }
-},{
-    timestamps: true
-});
+const productsSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: [{ type: String, required: true }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-const model = mongoose.model("produc", productsSchema);
+const model = mongoose.model("Product", productsSchema);
 
 module.exports = model;
